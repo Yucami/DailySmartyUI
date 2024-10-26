57a5023f020ce4e07ab4c091662fc5ab2dfb1823
odaya frauendorff navarro <odayafrauendorffnavarro@MacBook-Pro-de-odaya.local>
2024-10-26 08:25:47 +0200

    configuró herramientas de desarrollo redux y corrigió el reducer SET_RECENT_POSTS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
+import { SET_RECENT_POSTS } from "./types";
+
+import axios from "axios";
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-      //perform our request in here.
-      console.log("hello");
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response.data);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.posts
+                })
+            })
     }
 }
